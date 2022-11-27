87863b8691aedf572c69f89b7f68e5953be3dd2b
pascalburlet <pburlet@outlook.com>
2022-11-27 14:41:03 +0100

    implements a custom selector for clipped TextFields and TextViews

--- Sources/CustomKeyboardKit/ViewExtensions/View+CustomKeyboard.swift
+++ Sources/CustomKeyboardKit/ViewExtensions/View+CustomKeyboard.swift
@@ -10,14 +10,21 @@ import UIKit
 import SwiftUI
 import Introspect
 
-public extension View {
+public extension TextField {
     func customKeyboard(_ keyboardType: CustomKeyboard) -> some View {
         self
-            .modifier(CustomKeyboardModifier(keyboardType: keyboardType))
+            .modifier(CustomKeyboardModifierTextField(keyboardType: keyboardType))
     }
 }
 
-public struct CustomKeyboardModifier: ViewModifier {
+public extension TextEditor {
+    func customKeyboard(_ keyboardType: CustomKeyboard) -> some View {
+        self
+            .modifier(CustomKeyboardModifierTextEditor(keyboardType: keyboardType))
+    }
+}
+
+public struct CustomKeyboardModifierTextEditor: ViewModifier {
     @Environment(\.onSubmit) var onSubmit
     var keyboardType: CustomKeyboard
     
@@ -27,11 +34,82 @@ public struct CustomKeyboardModifier: ViewModifier {
     
     public func body(content: Content) -> some View {
         content
-            .introspectTextField { uiTextField in
+            .introspectTextViewWithClipping { uiTextView in
+                uiTextView.inputView = keyboardType.keyboardInputView
+            }
+            .onAppear {
+                self.keyboardType.onSubmit = onSubmit
+            }
+    }
+}
+
+public struct CustomKeyboardModifierTextField: ViewModifier {
+    @Environment(\.onSubmit) var onSubmit
+    var keyboardType: CustomKeyboard
+    
+    public init(keyboardType: CustomKeyboard) {
+        self.keyboardType = keyboardType
+    }
+    
+    public func body(content: Content) -> some View {
+        content
+            .introspectTextFieldWithClipping { uiTextField in
                 uiTextField.inputView = keyboardType.keyboardInputView
             }
             .onAppear {
                 self.keyboardType.onSubmit = onSubmit
             }
+    }
+}
+
+
+
+extension View {
+    //Makes it more reliable for UITextField if stylings are added
+    //A workaround for views that might have clipping, according to following issue
+    //https://github.com/siteline/SwiftUI-Introspect/issues/115#issuecomment-1013653286
+    public func introspectTextFieldWithClipping(customize: @escaping (UITextField) -> ()) -> some View {
+        return inject(UIKitIntrospectionView(
+            selector: { introspectionView in
+                guard let viewHost = Introspect.findViewHost(from: introspectionView) else {
+                    return nil
+                }
+                // first run Introspect as normal
+                if let selectedView = Introspect.previousSibling(containing: UITextField.self, from: viewHost) {
+                    return selectedView
+                } else if let superView = viewHost.superview {
+                    // if no view was found and a superview exists, search the superview as well
+                    return Introspect.previousSibling(containing: UITextField.self, from: superView)
+                } else {
+                    // no view found at all
+                    return nil
+                }
+            },
+            customize: customize
+        ))
+    }
+    
+    //Makes it more reliable for UITextView if stylings are added
+    //A workaround for views that might have clipping, according to following issue
+    //https://github.com/siteline/SwiftUI-Introspect/issues/115#issuecomment-1013653286
+    public func introspectTextViewWithClipping(customize: @escaping (UITextView) -> ()) -> some View {
+        return inject(UIKitIntrospectionView(
+            selector: { introspectionView in
+                guard let viewHost = Introspect.findViewHost(from: introspectionView) else {
+                    return nil
+                }
+                // first run Introspect as normal
+                if let selectedView = Introspect.previousSibling(containing: UITextView.self, from: viewHost) {
+                    return selectedView
+                } else if let superView = viewHost.superview {
+                    // if no view was found and a superview exists, search the superview as well
+                    return Introspect.previousSibling(containing: UITextView.self, from: superView)
+                } else {
+                    // no view found at all
+                    return nil
+                }
+            },
+            customize: customize
+        ))
     }
 }
